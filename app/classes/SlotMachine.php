958f38aae15840e39fec1b87ff0cee3fa818c7e5
Katamaranas <ernestas.zidokas@gmail.com>
2019-04-05 13:49:12 +0300

    slotmacine 3x3 padarytas

--- app/classes/SlotMachine.php
+++ app/classes/SlotMachine.php
@@ -2,7 +2,7 @@
 
 namespace App;
 
-class SlotMachine extends App\Abstracts\SlotMachine {
+class SlotMachine extends Abstracts\SlotMachine {
 
     public function __construct($bandymo_kaina, $eiluciu_skaicius, $stulpeliu_skaicius, $variantu_celeje_skaicius, $fairness_coficientas) {
         $this->bandymo_kaina = $bandymo_kaina;
@@ -16,7 +16,7 @@ class SlotMachine extends App\Abstracts\SlotMachine {
         $this->state = [];
 
         for ($i = 0; $i < $this->stulpeliu_skaicius; $i++) {
-            for ($j = 0; $j < 3; $i++) {
+            for ($j = 0; $j < 3; $j++) {
                 $this->state[$i][$j] = rand(1, $this->variantu_celeje_skaicius);
             }
         }
@@ -29,16 +29,24 @@ class SlotMachine extends App\Abstracts\SlotMachine {
     }
 
     public function Outcome() {
+        if ($this->isWin()) {
+            return $this->bandymo_kaina * $this->fairness_coficientas * ($this->variantu_celeje_skaicius ** $this->stulpeliu_skaicius);
+        }
+
+        return (-1) * $this->bandymo_kaina;
+    }
+    public function isWin() {
         $suma = 0;
         foreach ($this->getMiddleRow() as $value) {
             $suma += $value;
         }
         if ($suma % $this->stulpeliu_skaicius == 0) {
-            return $this->bandymo_kaina * $this->fairness_coficientas * ($this->variantu_celeje_skaicius ** $this->stulpeliu_skaicius);
+            return true;
         }
 
         return false;
     }
+
 
     public function getBandymoKaina() {
         return $this->bandymo_kaina;
--- public_html/slot3x3.php
+++ public_html/slot3x3.php
@@ -16,8 +16,11 @@ $form = [
         'fail' => []
     ]
 ];
-?>
+$slot3x3 = new App\SlotMachine3x3();
+$slot3x3->Shuffle();
 
+
+?>
 <html>
     <head>
         <title>Casino ple</title>
@@ -32,22 +35,19 @@ $form = [
                 </nav>
                 <h1>3x3</h1>
                 <div class="slotmachine">
-                    <div class="row">
-                        <div class="element element-1">1</div>
-                        <div class="element element-1">1</div>
-                        <div class="element element-1">1</div>
-                    </div>
-                    <div class="row">
-                        <div class="element element-1">1</div>
-                        <div class="element element-1">1</div>
-                        <div class="element element-1">1</div>
-                    </div>
-                    <div class="row">
-                        <div class="element element-1">1</div>
-                        <div class="element element-1">1</div>
-                        <div class="element element-1">1</div>
-                    </div>
+                    <?php foreach ($slot3x3->Shuffle() as $column): ?>
+                        <div class="row">
+                            <?php foreach ($column as $col_data): ?>
+                                <div class="element element-<?php print $col_data; ?>"></div>
+                            <?php endforeach; ?>
+                        </div>
+                    <?php endforeach; ?>
                 </div>
+                <?php if ($slot3x3->isWin()): ?>
+                    <h2>YOU WIN</h2>
+                <?php else: ?>
+                    <h2>PLAY AGAIN</h2>
+                <?php endif; ?>
                 <?php require '../core/views/form.php'; ?>
             </div>
         </div>
