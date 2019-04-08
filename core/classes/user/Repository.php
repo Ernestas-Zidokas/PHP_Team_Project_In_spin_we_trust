d2dbd71fc7a26660ac432f6c651b3d531db7f3b8
Katamaranas <ernestas.zidokas@gmail.com>
2019-04-08 13:53:47 +0300

    session class

--- core/classes/user/Repository.php
+++ core/classes/user/Repository.php
@@ -4,15 +4,9 @@ namespace Core\User;
 
 class Repository extends \Core\User\Abstracts\Repository {
 
-    /**
-     * Patikrinam are user'is su tokiu email'u egzistuoja
-     * Jeigu ne, tada įtraukiam jį į duombazę ir
-     * returniname REGISTER_SUCCESS
-     * Jeigu egzistuoja, returniname REGISTER_ERR_EXISTS
-     */
-    public function register(Core\User\User $user) {
-        if (!$this->db->rowExists($this->table_name, $user->getEmail())) {
-            $this->insert($user->getEmail(), $user);
+    public function register(\Core\User\User $user) {
+        if (!$this->exists($user)) {
+            $this->insert($user);
 
             return self::REGISTER_SUCCESS;
         }
